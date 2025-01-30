eba80cc28b41ec5bba696c9dea0a9fe6bb1f0f40
nvgudz <nvgudz@gmail.com>
2025-01-30 22:30:54 +0200

    add

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,7 +3,7 @@ import { User as Profile } from './User.jsx'
 export default function Gallery() {
   return (
     <section className='contentBlock'>
-      <h1>Amazing scientists add</h1>
+      <h1>Amazing scientists add +</h1>
       <Profile />
     </section>
   )
